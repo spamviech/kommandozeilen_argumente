03e48fba3fa01373c96aceffab7db22f6f573963
spamviech <spamviech@web.de>
2022-03-13 11:54:59 +0100

    english description in the docstring

--- src/sprache.rs
+++ src/sprache.rs
@@ -1,44 +1,97 @@
 //! Alle Strings, die zum erstellen von Hilfe-Text und Fehlermeldung notwendig sind.
 
-// TODO english doc for fields
 /// Alle Strings, die zum erstellen von Hilfe-Text und Fehlermeldung notwendig sind.
+///
+/// ## English synonym
+/// [Language]
 #[derive(Debug, Clone, Copy)]
 pub struct Sprache {
     /// Standard-Präfix zum invertieren einer Flag.
+    ///
+    /// ## English
+    /// Default prefix to invert a flag.
     pub invertiere_präfix: &'static str,
     /// Standard-Wert für die Meta-Variable im Hilfe-Text
+    ///
+    /// ## English
+    /// Default-value for the meta-variable in the help text.
     pub meta_var: &'static str,
     /// Meta-Beschreibung für Optionen im Hilfe-Text.
+    ///
+    /// ## English
+    /// Meta-description for options in the help text.
     pub optionen: &'static str,
     /// Beschreibung für Standard-Wert im Hilfe-Text.
+    ///
+    /// ## English
+    /// Description for the default value in the help text.
     pub standard: &'static str,
     /// Beschreibung für mögliche Werte im Hilfe-Text.
+    ///
+    /// ## English
+    /// Description for possible values in the help text.
     pub erlaubte_werte: &'static str,
     /// Beschreibung einer fehlenden Flag in einer Fehlermeldung.
+    ///
+    /// ## English
+    /// Description for a missing flag in an error message.
     pub fehlende_flag: &'static str,
     /// Beschreibung eines fehlenden Wertes in einer Fehlermeldung.
+    ///
+    /// ## English
+    /// Description for a missing value in an error message.
     pub fehlender_wert: &'static str,
     /// Beschreibung eines Parse-Fehlers in einer Fehlermeldung.
+    ///
+    /// ## English
+    /// Description for a parse error in an error message.
     pub parse_fehler: &'static str,
     /// Beschreibung eines invaliden Strings in einer Fehlermeldung.
+    ///
+    /// ## English
+    /// Description for an invalid String in an error message.
     pub invalider_string: &'static str,
     /// Beschreibung für ein nicht verwendetes Argument in einer Fehlermeldung.
+    ///
+    /// ## English
+    /// Description for an unused argument in an error message.
     pub argument_nicht_verwendet: &'static str,
     /// Beschreibung für die Hilfe-Flag im automatisch erzeugten Hilfe-Text.
+    ///
+    /// ## English
+    /// Description for the help flag in the automatically created help text.
     pub hilfe_beschreibung: &'static str,
     /// Lang-Name für die Hilfe-Flag.
+    ///
+    /// ## English
+    /// Long name for the help flag.
     pub hilfe_lang: &'static str,
     /// Kurz-Name für die Hilfe-Flag.
+    ///
+    /// ## English
+    /// Short name for the help flag.
     pub hilfe_kurz: &'static str,
     /// Beschreibung für die Version-Flag im automatisch erzeugten Hilfe-Text.
+    ///
+    /// ## English
+    /// Description for the version flag in the automatically created help text.
     pub version_beschreibung: &'static str,
     /// Lang-Name für die Version-Flag.
+    ///
+    /// ## English
+    /// Long name for the version flag.
     pub version_lang: &'static str,
     /// Kurz-Name für die Version-Flag.
+    ///
+    /// ## English
+    /// Short name for the version flag.
     pub version_kurz: &'static str,
 }
 
 /// All strings required to produce help text and error message.
+///
+/// ## Deutsches Synonym
+/// [Sprache]
 pub type Language = Sprache;
 
 impl Sprache {
