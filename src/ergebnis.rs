b78cd6ac9768cac57341a1be0f99c54ab1991540
spamviech <spamviech@web.de>
2022-03-13 16:20:02 +0100

    english doc/synonym for Error

--- src/ergebnis.rs
+++ src/ergebnis.rs
@@ -7,7 +7,6 @@ use nonempty::NonEmpty;
 
 use crate::sprache::{Language, Sprache};
 
-// TODO english doc for variants
 /// Ergebnis des Parsen von Kommandozeilen-Argumenten.
 ///
 /// ## English synonym
@@ -60,42 +59,86 @@ impl<T, E> Ergebnis<T, E> {
     }
 }
 
-// TODO english doc for variants
-/// Fehlerquellen beim Parsen von Kommandozeilen-Argumenten
+/// Fehlerquellen beim Parsen von Kommandozeilen-Argumenten.
+///
+/// ## English synonym
+/// [Result]
 #[derive(Debug, Clone)]
 pub enum Fehler<E> {
     /// Ein benötigtes Flag-Argument wurde nicht genannt.
+    ///
+    /// ## English
+    /// A required flag argument is missing.
     FehlendeFlag {
         /// Vollständiger Name.
+        ///
+        /// ## English
+        /// Full name.
         lang: NonEmpty<String>,
         /// Kurzform des Namen.
+        ///
+        /// ## English
+        /// Short form of the name.
         kurz: Vec<String>,
         /// Präfix zum invertieren.
+        ///
+        /// ## English
+        /// Prefix to invert the flag.
         invertiere_präfix: String,
     },
     /// Ein benötigtes Wert-Argument wurde nicht genannt.
+    ///
+    /// ## English
+    /// A required value argument is missing.
     FehlenderWert {
         /// Vollständiger Name.
+        ///
+        /// ## English
+        /// Full name.
         lang: NonEmpty<String>,
         /// Kurzform des Namen.
+        ///
+        /// ## English
+        /// Short form of the name.
         kurz: Vec<String>,
         /// Verwendete Meta-Variable für den Wert.
+        ///
+        /// ## English
+        /// Used Meta-variable of the value.
         meta_var: String,
     },
     /// Fehler beim Parsen des genannten Wertes.
+    ///
+    /// ## English
+    /// Error while parsing the value.
     Fehler {
         /// Vollständiger Name.
+        ///
+        /// ## English
+        /// Full name
         lang: NonEmpty<String>,
         /// Kurzform des Namen.
+        ///
+        /// ## English
+        /// Short form of the name.
         kurz: Vec<String>,
         /// Verwendete Meta-Variable für den Wert.
+        ///
+        /// ## English
+        /// Used Meta-variable of the value.
         meta_var: String,
         /// Beim Parsen aufgetretener Fehler.
+        ///
+        /// ## English
+        /// Reported error from parsing.
         fehler: ParseFehler<E>,
     },
 }
 
 /// Possible errors when parsing command line arguments.
+///
+/// ## Deutsches Synonym
+/// [Fehler]
 pub type Error<E> = Fehler<E>;
 
 pub(crate) fn namen_regex_hinzufügen(string: &mut String, head: &String, tail: &[String]) {
@@ -116,33 +159,53 @@ pub(crate) fn namen_regex_hinzufügen(string: &mut String, head: &String, tail: 
     }
 }
 
-// TODO english doc for variants
 /// Mögliche Fehler-Quellen beim Parsen aus einem [OsStr](std::ffi::OsStr).
+///
+/// ## English synonym
+/// [ParseError]
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ParseFehler<E> {
     /// Die Konvertierung in ein [&str](str) ist fehlgeschlagen.
+    ///
+    /// ## English
+    /// Conversion to a [&str](str) failed.
     InvaliderString(OsString),
     /// Fehler beim Parsen des Strings.
+    ///
+    /// ## English
+    /// Error while parsing the string.
     ParseFehler(E),
 }
 
 /// Possible errors when parsing an [OsStr](std::ffi::OsStr).
+///
+/// ## Deutsches Synonym
+/// [ParseFehler]
 pub type ParseError<E> = ParseFehler<E>;
 
 impl<E: Display> Fehler<E> {
     /// Zeige den Fehler in Menschen-lesbarer Form an.
+    ///
+    /// ## English version
+    /// [error_message](Error::error_message)
     #[inline(always)]
     pub fn fehlermeldung(&self) -> String {
         self.erstelle_fehlermeldung_mit_sprache(Sprache::DEUTSCH)
     }
 
     /// Show the error in a human readable form.
+    ///
+    /// ## Deutsches Version
+    /// [fehlermeldung](Fehler::fehlermeldung)
     #[inline(always)]
     pub fn error_message(&self) -> String {
         self.erstelle_fehlermeldung_mit_sprache(Language::ENGLISH)
     }
 
     /// Zeige den [Fehler] in Menschen-lesbarer Form an.
+    ///
+    /// ## English synonym
+    /// [create_error_message_with_language](Error::create_error_message_with_language)
     #[inline(always)]
     pub fn erstelle_fehlermeldung_mit_sprache(&self, sprache: Sprache) -> String {
         self.erstelle_fehlermeldung(
@@ -154,12 +217,18 @@ impl<E: Display> Fehler<E> {
     }
 
     /// Show the [Error] in human readable form.
+    ///
+    /// ## Deutsches Synonym
+    /// [erstelle_fehlermeldung_mit_sprache](Fehler::erstelle_fehlermeldung_mit_sprache)
     #[inline(always)]
     pub fn create_error_message_with_language(&self, language: Language) -> String {
         self.erstelle_fehlermeldung_mit_sprache(language)
     }
 
     /// Zeige den Fehler in Menschen-lesbarer Form an.
+    ///
+    /// ## English synonym
+    /// [create_error_message](Error::create_error_message)
     pub fn erstelle_fehlermeldung(
         &self,
         fehlende_flag: &str,
@@ -222,6 +291,9 @@ impl<E: Display> Fehler<E> {
     }
 
     /// Show the [Error] in human readable form.
+    ///
+    /// ## Deutsches Synonym
+    /// [erstelle_fehlermeldung](Fehler::erstelle_fehlermeldung)
     #[inline(always)]
     pub fn create_error_message(
         &self,
