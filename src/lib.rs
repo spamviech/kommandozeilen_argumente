d98769cf7a3a3de01249e98b3181e819523d198c
spamviech <spamviech@web.de>
2024-01-06 00:06:19 +0100

    transformiere in workspace, aktiviere lints in Cargo.toml
    Konfiguriere aktivierte clippy-lints, bearbeiten bei Gelegenheit.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,5 @@
 #![doc = include_str!("../LIESMICH.md")]
-// Aktiviere alle Warnungen außer box_pointers, non_ascii_idents, unstable_features
-#![warn(
-    absolute_paths_not_starting_with_crate,
-    elided_lifetimes_in_paths,
-    explicit_outlives_requirements,
-    keyword_idents,
-    macro_use_extern_crate,
-    meta_variable_misuse,
-    missing_abi,
-    missing_copy_implementations,
-    missing_debug_implementations,
-    missing_docs,
-    noop_method_call,
-    pointer_structural_match,
-    rust_2021_incompatible_closure_captures,
-    rust_2021_incompatible_or_patterns,
-    rust_2021_prefixes_incompatible_syntax,
-    rust_2021_prelude_collisions,
-    single_use_lifetimes,
-    trivial_casts,
-    trivial_numeric_casts,
-    unreachable_pub,
-    unsafe_code,
-    unsafe_op_in_unsafe_fn,
-    unused_crate_dependencies,
-    unused_extern_crates,
-    unused_import_braces,
-    unused_lifetimes,
-    unused_qualifications,
-    unused_results,
-    variant_size_differences
-)]
+
 // Verwende doc_cfg für bessere Dokumentation von feature-gated derive Macros.
 #![cfg_attr(all(doc, not(doctest)), feature(doc_cfg))]
 
