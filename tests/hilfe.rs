67e4697c7a6f2fe5298a430e6c2f56b4f8e2c424
spamviech <spamviech@web.de>
2024-05-07 13:51:00 +0200

    beispiel für Anwendung mit neuem API

--- tests/hilfe.rs
+++ tests/hilfe.rs
@@ -10,11 +10,18 @@
     clippy::use_debug
 )]
 
-use std::{ffi::OsString, iter, process};
+use std::{
+    convert::identity,
+    ffi::{OsStr, OsString},
+    iter, process,
+};
 
 use void::Void;
 
-use kommandozeilen_argumente::{Argumente, Beschreibung, Ergebnis, Sprache};
+use kommandozeilen_argumente::{
+    argumente::{einzelargument::EinzelArgument, flag::Flag, new},
+    Argumente, Beschreibung, Ergebnis, ParseFehler, Sprache,
+};
 
 #[test]
 fn hilfe_test() {
@@ -31,6 +38,45 @@ fn hilfe_test() {
         "0.test",
     );
     match arg.parse(iter::once(OsString::from("--hilfe".to_owned()))) {
+        (Ergebnis::FrühesBeenden(nachrichten), nicht_verwendet) => {
+            let übrige = nicht_verwendet.len();
+            if übrige > 0 {
+                eprintln!("Nicht verwendete Argumente: {nicht_verwendet:?}");
+                process::exit(1);
+            } else {
+                for nachricht in nachrichten {
+                    println!("{nachricht}");
+                }
+            }
+        },
+        res => {
+            eprintln!("Unerwartetes Ergebnis: {res:?}");
+            process::exit(2);
+        },
+    }
+}
+
+#[test]
+fn new_hilfe_test() {
+    let sprache = Sprache::DEUTSCH;
+    let flag: Flag<'_, _, fn(_) -> _, fn(&bool) -> String> = Flag {
+        beschreibung: Beschreibung::neu_mit_sprache(
+            "test".to_owned(),
+            None::<&str>,
+            Some("hilfe"),
+            Some(false),
+            sprache,
+        ),
+        invertiere_präfix: sprache.invertiere_präfix.into(),
+        invertiere_infix: sprache.invertiere_infix.into(),
+        konvertiere: identity,
+        anzeige: <bool as ToString>::to_string,
+    };
+    let einzelargument = EinzelArgument::Flag(flag);
+    let arg: new::Argumente<'_, _, _, fn(&OsStr) -> Result<bool, ParseFehler<Void>>, _, Void> =
+        new::Argumente::EinzelArgument(einzelargument);
+    // TODO erzeuge hilfe flag
+    match arg.parse(iter::once(Some(OsString::from("--hilfe".to_owned())))) {
         (Ergebnis::FrühesBeenden(nachrichten), nicht_verwendet) => {
             let übrige = nicht_verwendet.len();
             if übrige > 0 {
