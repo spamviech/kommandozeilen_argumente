59572a6c6724f41f718254dd7b024f5e83f7e24e
spamviech <spamviech@web.de>
2024-05-12 22:30:16 +0200

    entferne Konfiguration

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ pub use self::{
         wert::{EnumArgument, Value, Wert},
         Argumente, Arguments,
     },
-    beschreibung::{Beschreibung, Configuration, Description, Konfiguration},
+    beschreibung::{Beschreibung, Description},
     ergebnis::{Ergebnis, Error, Fehler, ParseError, ParseFehler, Result},
     parse::{Parse, ParseArgument},
     sprache::{Language, Sprache},
